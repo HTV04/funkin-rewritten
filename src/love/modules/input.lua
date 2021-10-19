1e5d24a8b491e1aa4ba7dfbd88a5fdbb097613e6
GameVasion <bradsnewphone2006@gmail.com>
2021-10-19 16:06:43 -0400

    Add dfjk support

--- src/love/modules/input.lua
+++ src/love/modules/input.lua
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ------------------------------------------------------------------------------]]
-
+--[[
 if love.system.getOS() == "NX" then
 	return baton.new {
 		controls = {
@@ -54,3 +54,4 @@ else
 		joystick = love.joystick.getJoysticks()[1]
 	}
 end
+]]--
